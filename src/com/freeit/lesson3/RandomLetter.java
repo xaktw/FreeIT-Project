082da1ce3878a1f025f3f27a7fb8bb4821218564
Dmitriy Tanenya <dmitriytanenja@gmail.com>
2022-07-02 12:12:56 +0300

    Task3

--- src/com/freeit/lesson3/RandomLetter.java
+++ src/com/freeit/lesson3/RandomLetter.java
@@ -2,6 +2,7 @@ package com.freeit.lesson3;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,4 +16,43 @@ public class RandomLetter {
      * При каждом вводе пользователем программа долна говорить "warmly, hot" или "chilly, cold"
      * в зависимости от того, как далеко буква пользователя от загаданной
      */
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Добро пожаловать игру 'отгадай букву'  " );
+
+        /*
+        блок создания произвольного числа
+         */
+        Random rand = new Random();
+        int randLetter = rand.nextInt(32);
+        int hiddenLetter = (int) (randLetter + 'а');
+        /*
+        let`s go
+         */
+        Scanner in = new Scanner(System.in);
+        boolean endGame = false;
+        while (!endGame) {
+            System.out.println("Введите букву [от а до я ]: ");
+            String userIn = in.nextLine().toLowerCase();
+            char userLetter = userIn.charAt(0);
+            int Letter = userLetter;
+            if (Letter == hiddenLetter) {
+                System.out.println("Поздравляем! Скрытая буква  '" + userLetter + "' Вы выйграли!");
+                endGame = true;
+            } else {
+                int difference = Math.abs(Letter - hiddenLetter);
+                if (difference > 10) {
+                    System.out.println("холодно");
+                } else if (difference > 5) {
+                    System.out.println("теплее");
+                } else if (difference > 2) {
+                    System.out.println("горячо");
+                } else {
+                    System.out.println("очень даже рядом");
+                }
+            }
+        }
+    }
 }
